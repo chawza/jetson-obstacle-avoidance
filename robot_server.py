322e164588266caa7407c2d70fca58727277c112
Nabeel <nabeelkahili403@gmail.com>
2022-03-06 23:37:04 +0700

    robot task splits into to process

--- robot_server.py
+++ robot_server.py
@@ -1,110 +1,132 @@
-from distutils.log import error
-from threading import Thread
-import time
+from threading import Thread, Event
+from multiprocessing import Process
 from getch import getch
-import signal
 import cv2
-import numpy as np
 import websockets
-from websockets.server import WebSocketServerProtocol, serve, unix_serve
+from websockets.exceptions import ConnectionClosedOK
+from websockets.server import WebSocketServerProtocol, serve
 from Jetson import GPIO
 from robot import Robot, Action as RobotAction
 import asyncio
+from capture_cam import StereoCams
+from calibration_server import decode_img_to_byte
 
-left_cam_idx = 0
-right_cam_idx = 1
+left_img = None
+right_img = None
 host='192.168.100.11'
-port=8765
-ws_server = None
+# port=8765
 robot = Robot()
-stop_robot = asyncio.Event()
+app_stop = None
+cam_stop = None
 
-# def watch_keyboard():
-#   print('press "q" to quit')
-#   while True:
-#     time.sleep(1)
-#     key = getch()
-#     if key == 'q':
-#       break
-#   print('exit robot')
-#   stop_robot.set()
-#   return None
+def read_cam_task():
+  global left_img
+  global right_img
+  camera = StereoCams()
+  for left, right in camera.read(time_split=.001):
+    left_img, right_img = left, right
+  camera.clean_up()
 
 async def listen_controller(websocket: WebSocketServerProtocol):
+  global app_stop
   try:
     global robot
     async for data in websocket:
-      print('Client > {}'.format(data))
       action = data
       if action == RobotAction.forward:
+        print('ROBOT: FORWARD')
         robot.forward()
         await websocket.send('success')
       elif action == RobotAction.backward:
+        print('ROBOT: BACKWARD')
         robot.backward()
       elif action == RobotAction.left:
+        print('ROBOT: LEFT')
         robot.left()
       elif action == RobotAction.right:
+        print('ROBOT: RIGHT')
         robot.right()
       elif action == RobotAction.stop:
+        print('ROBOT: STOP')
         robot.stop()
       elif action == 'QUIT':
         robot.quit()
-        stop_robot.set()
         await websocket.close()
+        app_stop.set()
       else:
         print('WHAT', action)
         error_message = f'Unrecognized Action "{action}"'
-        await websocket.send(error_message)
+        print(error_message)
 
   except Exception as error:
     print(error)
     robot.stop()
+    app_stop.set()
+
 
 async def broadcast_cam(websocket: WebSocketServerProtocol):
-  global robot
-  global stop_robot
-  cam_left = cv2.VideoCapture(left_cam_idx)
-  cam_right = cv2.VideoCapture(right_cam_idx)
-
-  if cam_left.isOpened() and cam_right.isOpened():
-    print('cam opened')
-    while True and not stop_robot.is_set():
-      _, left_img = cam_left.read()
-      _, right_img = cam_right.read()
-
-      merged_img = np.hstack((left_img, right_img))
-
-      buffer_img = cv2.imencode('.jpg', merged_img)[1]
-      await websocket.send(buffer_img.tobytes())
-      asyncio.sleep(.01)
-  else:
-    error_message = 'Cannot open Camera'
-    await websocket.send(error_message)
-    robot.quit()
-
-  cam_left.release()
-  cam_right.release()
+  print('borad cast cam in port {}'.format(websocket.port))
+  global left_img
+  while True:
+    if left_img is not None:
+      grey_img = cv2.cvtColor(left_img, cv2.COLOR_BGR2GRAY)
+      grey_img = cv2.resize(grey_img, dsize=(
+        round(grey_img.shape[1]/3),
+        round(grey_img.shape[0]/3),
+      ))
+      byte_img = decode_img_to_byte(grey_img)
+      await websocket.send(byte_img)
+    asyncio.sleep(0.01)
 
 
-async def ws_handler(websocket: WebSocketServerProtocol, path):
-  print('Client make connection from in {}'.format(websocket.path))
-  if websocket.path == '/':
-    await listen_controller(websocket)
-  if websocket.path == '/cam':
-    await broadcast_cam(websocket)
+async def ws_handler(websocket: WebSocketServerProtocol, _):
+  path = websocket.path
+  global app_stop
+  try:
+    print('{} starts'.format(path))
+    if path == '/command':
+      await listen_controller(websocket)
+    if path == '/cam':
+      await broadcast_cam(websocket)
+  except ConnectionClosedOK:
+    print('{} stops'.format(path))
 
-async def initiate_server():
-  global ws_server
-  global stop_robot
-   
-  async with serve(ws_handler, host, port) as server:
-    ws_server = server
-    await stop_robot.wait()
+  finally:
+    if path == '/command':
+      app_stop.set()
+
+async def app_server():
+  global app_stop
+  curr_lopp = asyncio.get_event_loop()
+  app_stop = asyncio.Event(loop=curr_lopp)
+
+  async with serve(ws_handler, host, 8765):
+    await app_stop.wait()
+
+async def cam_server():
+  print('cam server')
+  global cam_stop
+  curr_lopp = asyncio.get_event_loop()
+  cam_stop = asyncio.Event(loop=curr_lopp)
+
+  async with serve(ws_handler, host, 8766):
+    await cam_stop.wait()
+
+def cam_process_task():
+  capture_thread = Thread(target=read_cam_task)
+  capture_thread.start()
+  loop = asyncio.new_event_loop()
+  loop.run_until_complete(cam_server())
+  capture_thread.join()
 
 if __name__ == '__main__':
+
+  cam_process = Process(target=cam_process_task)
+  cam_process.start()
+
   loop = asyncio.get_event_loop()
-  loop.run_until_complete(initiate_server())  
-  loop.close()
+  loop.run_until_complete(app_server())  
+  
 
   if not robot.is_cleaned_up:
     robot.quit()
