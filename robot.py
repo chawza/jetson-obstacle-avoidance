331d48996c5f1e1cb672ee3f32351d9bb16ce22e
Nabeel <nabeelkahili403@gmail.com>
2022-03-06 16:48:35 +0700

    seperate robot as class and create more threads

---
+++ robot.py
@@ -0,0 +1,147 @@
+import sys
+from Jetson import GPIO
+import time
+from threading import Thread
+from getch import getch
+
+class Pin:
+  left_power = 16
+  left_direction = 15
+  right_power = 12
+  right_direction = 11
+
+class Action:
+  forward = 'FORWARD'
+  backward = 'BACKWARD'
+  left = 'LEFT'
+  right = 'RIGHT'
+  stop = 'STOP'
+
+class Robot:
+  def __init__(self):
+    print('Robot Activated')
+    self.current_action = ''
+    self.is_cleaned_up = False
+    self._setup_pins()
+  
+  def _setup_pins(self):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(
+      [
+        Pin.left_power,
+        Pin.left_direction,
+        Pin.right_power,
+        Pin.right_direction
+      ],
+      GPIO.OUT, initial=GPIO.LOW
+    )
+
+  def forward(self):
+    print('ROBOT: FORWARD')
+    GPIO.output(
+      [
+        Pin.left_power,
+        Pin.right_power,
+        Pin.left_direction
+      ],
+      GPIO.HIGH
+    )
+    GPIO.output(
+      Pin.right_direction,
+      GPIO.LOW
+    )
+    self.current_action = Action.forward
+
+  def backward(self):
+    print('ROBOT: BACKWARD')
+    GPIO.output(
+      [
+        Pin.left_power,
+        Pin.right_power,
+        Pin.right_direction,
+      ],
+      GPIO.HIGH
+    )
+    GPIO.output(
+      Pin.left_direction,
+      GPIO.LOW
+    )
+    self.current_action = Action.backward
+  
+  def left(self):
+    print('ROBOT: LEFT')
+    GPIO.output(
+      [
+        Pin.left_power,
+        Pin.right_power,
+      ],
+      GPIO.HIGH
+    )
+    GPIO.output(
+      [
+        Pin.left_direction,
+        Pin.right_direction
+      ],
+      GPIO.LOW
+    )
+    self.current_action = Action.left
+  
+  def right(self):
+    print('ROBOT: right')
+    GPIO.output(
+      [
+        Pin.left_power,
+        Pin.right_power,
+      ],
+      GPIO.HIGH
+    )
+    GPIO.output(
+      [
+        Pin.left_direction,
+        Pin.right_direction
+      ],
+      GPIO.HIGH
+    )
+    self.current_action = Action.right
+
+  def stop(self):
+    print('ROBOT: STOP')
+    self._setup_pins()
+    self.current_action = Action.stop
+
+  def quit(self):
+    self.stop()
+    print("Robot Deactivated")
+    GPIO.cleanup()
+    self.is_cleaned_up = True
+
+def app():
+  print('APP RUNS')
+  robot = Robot()
+  running = True
+  curr_key = ''
+  
+  while running:
+    time.sleep(.001)
+    key = getch()
+
+    if key.lower() != curr_key:
+      curr_key = key.lower()
+      if curr_key == 'w':
+        robot.forward()
+        print(Action.forward)
+      elif curr_key == 's':
+        robot.backward()
+        print(Action.backward)
+      elif curr_key == 'e':
+        robot.stop()
+        print(Action.stop)
+      else:
+        print(f'invalid input: {key}')
+        robot.stop()
+    
+    if key == 'q' or key == ' ':
+      running = False
+  
+  print('APP STOPS')
+  GPIO.cleanup()
--- robot_server.py
+++ robot_server.py
@@ -1,48 +1,110 @@
+from distutils.log import error
+from threading import Thread
+import time
+from getch import getch
 import signal
 import cv2
+import numpy as np
 import websockets
 from websockets.server import WebSocketServerProtocol, serve, unix_serve
 from Jetson import GPIO
 from robot import Robot, Action as RobotAction
 import asyncio
 
+left_cam_idx = 0
+right_cam_idx = 1
 host='192.168.100.11'
 port=8765
 ws_server = None
 robot = Robot()
+stop_robot = asyncio.Event()
+
+# def watch_keyboard():
+#   print('press "q" to quit')
+#   while True:
+#     time.sleep(1)
+#     key = getch()
+#     if key == 'q':
+#       break
+#   print('exit robot')
+#   stop_robot.set()
+#   return None
+
+async def listen_controller(websocket: WebSocketServerProtocol):
+  try:
+    global robot
+    async for data in websocket:
+      print('Client > {}'.format(data))
+      action = data
+      if action == RobotAction.forward:
+        robot.forward()
+        await websocket.send('success')
+      elif action == RobotAction.backward:
+        robot.backward()
+      elif action == RobotAction.left:
+        robot.left()
+      elif action == RobotAction.right:
+        robot.right()
+      elif action == RobotAction.stop:
+        robot.stop()
+      elif action == 'QUIT':
+        robot.quit()
+        stop_robot.set()
+        await websocket.close()
+      else:
+        print('WHAT', action)
+        error_message = f'Unrecognized Action "{action}"'
+        await websocket.send(error_message)
+
+  except Exception as error:
+    print(error)
+    robot.stop()
+
+async def broadcast_cam(websocket: WebSocketServerProtocol):
+  global robot
+  global stop_robot
+  cam_left = cv2.VideoCapture(left_cam_idx)
+  cam_right = cv2.VideoCapture(right_cam_idx)
+
+  if cam_left.isOpened() and cam_right.isOpened():
+    print('cam opened')
+    while True and not stop_robot.is_set():
+      _, left_img = cam_left.read()
+      _, right_img = cam_right.read()
+
+      merged_img = np.hstack((left_img, right_img))
+
+      buffer_img = cv2.imencode('.jpg', merged_img)[1]
+      await websocket.send(buffer_img.tobytes())
+      asyncio.sleep(.01)
+  else:
+    error_message = 'Cannot open Camera'
+    await websocket.send(error_message)
+    robot.quit()
+
+  cam_left.release()
+  cam_right.release()
+
 
 async def ws_handler(websocket: WebSocketServerProtocol, path):
-  print('PATH: {}'.format(path))
-  global robot
-  print(f'Client make connection from {websocket.host} in port {websocket.port}')
-  async for data in websocket:
-    print('Client > {}'.format(data))
-    action = data
-    if action == RobotAction.forward:
-      robot.forward()
-    elif action == RobotAction.backward:
-      robot.backward()
-    elif action == RobotAction.stop:
-      robot.stop()
-    elif action == 'QUIT':
-      robot.quit()
-      await websocket.close()
-    else:
-      error_message = f'Unrecognized Action "{action}"'
-      await websocket.send('message:{}'.format(error_message))
+  print('Client make connection from in {}'.format(websocket.path))
+  if websocket.path == '/':
+    await listen_controller(websocket)
+  if websocket.path == '/cam':
+    await broadcast_cam(websocket)
 
 async def initiate_server():
   global ws_server
-  
+  global stop_robot
+   
   async with serve(ws_handler, host, port) as server:
     ws_server = server
-    print('serving server in')
-    await asyncio.Future()
-
+    await stop_robot.wait()
 
 if __name__ == '__main__':
   loop = asyncio.get_event_loop()
-  loop.run_until_complete(initiate_server())
+  loop.run_until_complete(initiate_server())  
   loop.close()
 
-
+  if not robot.is_cleaned_up:
+    robot.quit()
