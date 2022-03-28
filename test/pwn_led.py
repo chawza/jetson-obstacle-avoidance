76d83325beee7f44c00d8e9605d5905f650affb8
Nabeel <nabeelkahlil403@gmail.com>
2022-03-28 12:42:23 +0700

    test pwm to led

---
+++ test/pwn_led.py
@@ -0,0 +1,31 @@
+from Jetson import GPIO
+import time
+from getch import getch
+
+GPIO.setmode(GPIO.BOARD)
+
+pin_pwm0 = 32
+pin_pwm2 = 33
+
+GPIO.setup(pin_pwm0, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(pin_pwm2, GPIO.OUT, initial=GPIO.LOW)
+
+pin_1 = GPIO.PWM(pin_pwm0, 500)
+pin_2 = GPIO.PWM(pin_pwm2, 500)
+
+percentage = 0
+
+pin_1.start(percentage)
+pin_2.start(percentage)
+
+try:
+  while True:
+    if percentage >= 100:
+      break
+    percentage += 5
+    pin_1.ChangeDutyCycle(percentage)
+    pin_2.ChangeDutyCycle(percentage)
+    print('Percentage: {}'.format(percentage))
+    time.sleep(.5)
+finally:
+  GPIO.cleanup()
