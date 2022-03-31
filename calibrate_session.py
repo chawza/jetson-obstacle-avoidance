3d520afd8f27f488753177b21abd4ef6388d68d0
NabeelKM <nabeelkahlil403@gmail.com>
2022-04-01 00:00:27 +0700

    clean typing

--- calibrate_session.py
+++ calibrate_session.py
@@ -2,19 +2,16 @@ import cv2
 import numpy as np
 import calibration
 from capture_cam import StereoCams
-import os
 
 calibration.setup_img_save_directory()
 counter = calibration.initiate_img_counter()
 cam_preset = calibration.load_calibrate_map_preset()
-is_calibrate = False
+is_calibrate = True
 camera = StereoCams(left_idx=0, right_idx=2)
 
 for left, right in camera.read():
-  right = cv2.flip(right, -1)
   if is_calibrate:
     left, right = calibration.calibrate_imgs(left, right, cam_preset)
-    
 
   img = np.hstack((left, right))
 
@@ -30,3 +27,5 @@ for left, right in camera.read():
   if key == ord('c'):
     is_calibrate = not is_calibrate
     print(f'Calibrate: {is_calibrate}')
+
+camera.clean_up()
