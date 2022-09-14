1eda5604f5e58bd93d5930014e03816be48a9fdf
Nabeel <nabeelkahlil403@gmail.com>
2022-09-14 11:27:45 +0700

    adjust robot signal parameter

--- test/gpio_test.py
+++ test/gpio_test.py
@@ -2,16 +2,20 @@ import Jetson.GPIO as GPIO
 import time
 
 def blinking_led_light():
-  pin = 33
+  pin = 21
+  pin2 = 22
 
   GPIO.setmode(GPIO.BOARD)
   GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+  GPIO.setup(pin2, GPIO.OUT, initial=GPIO.LOW)
 
   for i in range(1,10 + 1):
     print(f'second: {i}')
     GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(pin2, GPIO.HIGH)
     time.sleep(.5)
     GPIO.output(pin, GPIO.LOW)
+    GPIO.output(pin2, GPIO.LOW)
     time.sleep(.5)
 
 if __name__ == '__main__':
